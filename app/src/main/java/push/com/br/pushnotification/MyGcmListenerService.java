793255e54f4405a106ef5372cdc0acd92a23f228
Jefferson Almeida <jefferson@hanzo.com.br>
2016-04-15 17:46:15 -0300

    [ Receiving Messages ] - Handling Messages
          * Implements GcmListenerService onMessageReceived
          * Sends notifications
    
    Closes #6

--- app/src/main/java/push/com/br/pushnotification/MyGcmListenerService.java
+++ app/src/main/java/push/com/br/pushnotification/MyGcmListenerService.java
@@ -1,5 +1,15 @@
 package push.com.br.pushnotification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
 import com.google.android.gms.gcm.GcmListenerService;
 
 /**
@@ -7,8 +17,48 @@ import com.google.android.gms.gcm.GcmListenerService;
  */
 public class MyGcmListenerService extends GcmListenerService {
 
+    private static final String TAG = MyGcmListenerService.class.getSimpleName();
+
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        super.onMessageReceived(from, data);
+        
+        String message = (String) data.get("message");
+
+        Log.e(TAG, "FROM: " + from);
+        Log.e(TAG, "MESSAGE: " + message);
+
+        sendNotification(message);
+
+    }
+    /**
+     * Create and show a simple notification containing the received GCM message.
+     *
+     * @param message GCM message received.
+     */
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("GCM Message")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
