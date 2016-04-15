f2625afa19da3781ee3950d59f0a7b62168112c3
Jefferson Almeida <jefferson@hanzo.com.br>
2016-04-15 12:22:03 -0300

    [ Set Up Google Play Services ] - Dependencies and Manifest
          * Add the google-services to your app-vel build.gradle
          * Add to the Manifest: uses-permission, GcmReceiver,
          * GcmListenerService, InstanceIDListenerService
    
    Closes #3

---
+++ app/src/main/java/push/com/br/pushnotification/MyGcmListenerService.java
@@ -0,0 +1,14 @@
+package push.com.br.pushnotification;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+/**
+ * Created by jeffersonalmeida on 4/15/16.
+ */
+public class MyGcmListenerService extends GcmListenerService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/push/com/br/pushnotification/MyInstanceIDListenerService.java
@@ -0,0 +1,14 @@
+package push.com.br.pushnotification;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+/**
+ * Created by jeffersonalmeida on 4/15/16.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
